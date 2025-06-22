a5fb0a8f558d13c6fc2507a2fcaa173ac3510b24
Pavan <izon2708@gmail.com>
2025-06-22 15:31:00 +0530

    Updating Code

--- Unit_Converter.java
+++ Unit_Converter.java
@@ -13,25 +13,30 @@ public class Unit_Converter extends JFrame {
             setLayout(new GridBagLayout());
 
             //Labels
-            titlel = new JLabel("*** UNIT CONVERTER ***")
+            titlel = new JLabel("*** UNIT CONVERTER ***");
             cml = new JLabel("Centimeter (cm) : ");
             feetl = new JLabel("Feet (ft) : ");
             inchl = new JLabel("Inch (in) : ");
             meterl = new JLabel("Meter (m) : ");
 
             //TextFields
-            cmt = new JTextField(20);
-            feett = new JTextField(20);
-            incht = new JTextField(20);
-            metert = new JTextField(20);
+            cmt = new JTextField(10);
+            feett = new JTextField(10);
+            incht = new JTextField(10);
+            metert = new JTextField(10);
 
             //Button
             convertbutton = new JButton("Convert");
 
-            GBadd(titlel,1,1,0,0);
-            GBadd(cml,1,3,0,0);
-            GBadd(cmt,2,3,0,0);
-            GBadd(feetl,1,5,0,0);
+            GBadd(titlel,1,1,1,1);
+            GBadd(cml,1,3,1,1);
+            GBadd(cmt,2,3,1,1);
+            GBadd(feetl,1,5,1,1);
+            GBadd(feett,2,5,1,1);
+            GBadd(inchl, 1,7,1,1);
+            GBadd(incht,2,7,1,1);
+            GBadd(meterl,1,9,1,1);
+            GBadd(metert,2,9,1,1);
 
         }
 
@@ -44,4 +49,9 @@ public class Unit_Converter extends JFrame {
             add(c,gbc);
         }
 
+    public static void main(String[] args) {
+        Unit_Converter uc = new Unit_Converter("Unit Converter");
+        uc.setVisible(true);
+        uc.setSize(500,500);
+    }
 }
